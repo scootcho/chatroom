b765a756372d0707fd55808ca2a8f40f2ef69eaa
Scott Yu <scott.tj.yu@gmail.com>
2015-01-02 09:51:38 +0800

    initialize repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Chatroom::Application.config.secret_token = '7a15dd8412bf519cd7990c7170ca21974f1c22cf1d947f3e4c560a0cd0abb0e401ddbf3043c457e586b2a6f774686a25a6a1949cc05b8a312eeacd79516db433'
